de5544bea3c16bd1c480e4a9ce348caf28193cfa
philmetzger <truetaurus@gmail.com>
2026-01-21 08:50:18 +0100

    basic editor

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <main className="w-full max-w-5xl px-8">
         <header className="mb-16 text-center">
           <h1 className="text-4xl font-light tracking-tight text-[rgba(25,25,25,0.6)]">
-            Ellipsus Coding Challenge - TipTap Spellchecker
+            Ellipsus Coding Challenge - Tiptap Spellchecker
           </h1>
         </header>
         <div className="tiptap-container rounded-3xl border border-[rgba(0,0,0,0.04)] bg-white">
