38a75e44f32bd7aed99d4ab02927087bdb0aab48
philmetzger <truetaurus@gmail.com>
2026-01-21 08:43:36 +0100

    basic editor

--- app/components/Editor/index.ts
+++ app/components/Editor/index.ts
@@ -1,16 +1,2 @@
 export { Editor, default } from './Editor'
-export { EditorPlaceholder } from './EditorPlaceholder'
-export { EditorContentWrapper } from './EditorContentWrapper'
-export { useEditor } from './hooks/useEditor'
-export { useEditorMount } from './hooks/useEditorMount'
-export { getExtensions, getBaseExtensions, configurePlaceholder } from './utils/getExtensions'
-export { defaultEditorConfig, sizeConfig, defaultEditorProps } from './utils/editorConfig'
-export type {
-  EditorProps,
-  EditorConfig,
-  EditorSize,
-  UseEditorOptions,
-  UseEditorReturn,
-  EditorPlaceholderProps,
-  EditorContentWrapperProps,
-} from './types/editor.types'
+export type { EditorProps } from './types/editor.types'
